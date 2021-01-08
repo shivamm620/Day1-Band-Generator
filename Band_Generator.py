ad882e3c12a32c657c780c7c726b81e5a1585141
Shivam Mishra <shivammishra7763914693@gmail.com>
2021-01-08 20:16:02 +0530

    band_generator

---
+++ Band_Generator.py
@@ -0,0 +1,7 @@
+print("Welcome to the band name genertor.")
+
+user_city = input("In which city you grew up in? \n")
+
+user_pet = input("What is your pet name? \n")
+
+print("Your band name could be"+user_city+ " " + user_pet )
